bd078efdbd752f762d887c339c165839caa232dd
LinC3e <ivancostilla17@gmail.com>
2024-04-10 14:19:22 -0300

    ADD - complete layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-3xl text-orange-600">Hello World</div>
+    <>
+    <Header showBackArrow label='Home'/>
+    </>
   )
 }
