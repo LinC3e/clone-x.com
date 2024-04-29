b2daeb36b9b4d505c4c720ec9963f07e098115da
LinC3e <ivancostilla17@gmail.com>
2024-04-29 18:53:22 -0300

    [FIX] | Show modals when click tweet btn

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,10 +1,18 @@
+import useLoginModel from "@/hooks/useModelLogin";
 import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 
 const SidebarTweetButton = () => {
     const router = useRouter();
+    const LoginModal = useLoginModel();
+
+    const onClick = useCallback(() => {
+        LoginModal.onOpen();
+    },[LoginModal]);
+
     return (
-        <div onClick={() => router.push('/')}>
+        <div onClick={onClick}>
             <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500
             hover:bg-opacity-50 transition cursor-pointer">
                 <FaFeather size={24} color='white' />
